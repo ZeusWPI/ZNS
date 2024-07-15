c213ac14f9fe41859cea65b37e1b6111d45287f9
Xander Bil <xander@biltopia.org>
2024-07-15 22:29:00 +0200

    change port

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ mod utils;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     Config::initialize();
-    let resolver_add = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let resolver_add = SocketAddr::from(([127, 0, 0, 1], 5353));
     let _ = tokio::join!(
         udp_listener_loop(resolver_add),
         tcp_listener_loop(resolver_add)
