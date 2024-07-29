c766d4631f239cd699d1243a9e4055df18816229
Xander Bil <xander@biltopia.org>
2024-07-29 21:46:56 +0200

    use cargo workspaces and add cli crate

--- zns-daemon/src/main.rs
+++ zns-daemon/src/main.rs
@@ -1,24 +1,14 @@
 use std::{error::Error, net::SocketAddr};
 
 use config::Config;
+use zns::config;
 
-use crate::resolver::{tcp_listener_loop, udp_listener_loop};
-
-mod config;
-mod db;
-mod errors;
-mod handlers;
-mod message;
-mod parser;
-mod reader;
-mod resolver;
-mod structs;
-mod utils;
+use zns::resolver::{tcp_listener_loop, udp_listener_loop};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     Config::initialize();
-    let resolver_add = SocketAddr::from(([127, 0, 0, 1], 5353));
+    let resolver_add = SocketAddr::from(([127, 0, 0, 1], 8080));
     let _ = tokio::join!(
         udp_listener_loop(resolver_add),
         tcp_listener_loop(resolver_add)
---
+++ zns/src/lib.rs
@@ -0,0 +1,11 @@
+mod db;
+mod handlers;
+mod message;
+mod reader;
+mod structs;
+mod utils;
+
+pub mod config;
+pub mod errors;
+pub mod parser;
+pub mod resolver;
